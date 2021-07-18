88db981d4245880adf375be4b42d69a89282c9e8
Adnan <adnan.goulamaly@gmail.com>
2021-07-18 14:21:02 +0400

    fature: ajout d'un systeme de validation de login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
 
 @Controller('/auth')
@@ -9,6 +9,20 @@ export class AuthController {
     // TODO: Validation du mdp de l'utilisateur à ajouter avan tde générer le token 
     @Post('/login')
     login(@Body() user) {
-       return this.authService.generateAccessToken(user);
+        return this.authService.validateUser(user.username,user.password)
+        .then((user)=>{
+            console.log(user);
+            // A commenter pour avoir la possiblité de se login avec n'importe quel 
+            // username + password
+            if(!user){
+                throw new UnauthorizedException();
+            }
+            return this.authService.generateAccessToken(user);
+
+        })
+        .catch((err)=>{
+            console.log(err);
+            throw new UnauthorizedException();
+        });
     }
 }
